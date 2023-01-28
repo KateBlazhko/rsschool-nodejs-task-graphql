59c4eedc12d9c0fdf6fd8a1dfc737598c0d1ef96
KateBlazhko <kateugnicheva@gmail.com>
2023-01-28 22:25:34 +0300

    feat: implement create, update request

--- src/routes/graphql/model.ts
+++ src/routes/graphql/model.ts
@@ -1,23 +1,16 @@
 import { GraphQLFloat, GraphQLInputObjectType, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
 
-export const EntityType = new GraphQLObjectType({
-  name: 'Entity',
-  fields: () => ({
-    users: {
-      type: UserType,
-
-    },
-    posts: {
-      type: PostType,
-    },
-    profiles: {
-      type: ProfileType,
-    },
-    memberTypeTypes: {
-      type: MemberTypeType,
-    },
-  }),
-});
+const userDTO = {
+  firstName: {
+    type: GraphQLString,
+  },
+  lastName: {
+    type: GraphQLString,
+  },
+  email: {
+    type: GraphQLString,
+  },
+};
 
 export const UserType = new GraphQLObjectType({
   name: 'User',
@@ -25,36 +18,38 @@ export const UserType = new GraphQLObjectType({
     id: {
       type: new GraphQLNonNull(GraphQLString),
     },
-    firstName: {
-      type: GraphQLString,
-    },
-    lastName: {
-      type: GraphQLString,
-    },
-    email: {
-      type: GraphQLString,
-    },
+    ...userDTO,
     subscribedToUserIds: {
       type: new GraphQLList(GraphQLString),
     },
   }),
 });
 
-export const UserDTOType = new GraphQLInputObjectType({
-  name: 'UserDTO',
+export const UserCreateType = new GraphQLInputObjectType({
+  name: 'UserCreate',
   fields: () => ({
-    firstName: {
-      type: GraphQLString,
-    },
-    lastName: {
-      type: GraphQLString,
-    },
-    email: {
-      type: GraphQLString,
+    ...userDTO
+  }),
+});
+
+export const UserChangeType = new GraphQLInputObjectType({
+  name: 'UserChange',
+  fields: () => ({
+    ...userDTO,
+    subscribedToUserIds: {
+      type: new GraphQLList(GraphQLString),
     },
   }),
 });
 
+const postDTO = {
+  title: {
+    type: GraphQLString,
+  },
+  content: {
+    type: GraphQLString,
+  },
+}
 
 export const PostType = new GraphQLObjectType({
   name: 'Post',
@@ -62,33 +57,53 @@ export const PostType = new GraphQLObjectType({
     id: {
       type: new GraphQLNonNull(GraphQLString),
     },
-    title: {
-      type: GraphQLString,
-    },
-    content: {
-      type: GraphQLString,
-    },
+    ...postDTO,
     userId: {
       type: new GraphQLNonNull(GraphQLString),
     },
   }),
 });
 
-export const PostDTOType = new GraphQLInputObjectType({
-  name: 'PostDTO',
+export const PostCreateType = new GraphQLInputObjectType({
+  name: 'PostCreate',
   fields: () => ({
-    title: {
-      type: GraphQLString,
-    },
-    content: {
-      type: GraphQLString,
-    },
+    ...postDTO, 
     userId: {
       type: new GraphQLNonNull(GraphQLString),
     },
   }),
 });
 
+export const PostChangeType = new GraphQLInputObjectType({
+  name: 'PostChange',
+  fields: () => ({
+    ...postDTO, 
+  }),
+});
+
+const profileDTO = {
+  avatar: {
+    type: GraphQLString,
+  },
+  sex: {
+    type: GraphQLString,
+  },
+  birthday: {
+    type: GraphQLInt,
+  },
+  country: {
+    type: GraphQLString,
+  },
+  street: {
+    type: GraphQLString,
+  },
+  city: {
+    type: GraphQLString,
+  },
+  memberTypeId: {
+    type: new GraphQLNonNull(GraphQLString),
+  },
+}
 
 export const ProfileType = new GraphQLObjectType({
   name: 'Profile',
@@ -96,62 +111,38 @@ export const ProfileType = new GraphQLObjectType({
     id: {
       type: new GraphQLNonNull(GraphQLString),
     },
-    avatar: {
-      type: GraphQLString,
-    },
-    sex: {
-      type: GraphQLString,
-    },
-    birthday: {
-      type: GraphQLInt,
-    },
-    country: {
-      type: GraphQLString,
-    },
-    street: {
-      type: GraphQLString,
-    },
-    city: {
-      type: GraphQLString,
-    },
-    memberTypeId: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
+    ...profileDTO,
     userId: {
       type: new GraphQLNonNull(GraphQLString),
     },
   }),
 });
 
-export const ProfileDTOType = new GraphQLInputObjectType({
-  name: 'ProfileDTO',
+export const ProfileCreateType = new GraphQLInputObjectType({
+  name: 'ProfileCreate',
   fields: () => ({
-    avatar: {
-      type: GraphQLString,
-    },
-    sex: {
-      type: GraphQLString,
-    },
-    birthday: {
-      type: GraphQLInt,
-    },
-    country: {
-      type: GraphQLString,
-    },
-    street: {
-      type: GraphQLString,
-    },
-    city: {
-      type: GraphQLString,
-    },
-    memberTypeId: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
+    ...profileDTO,
     userId: {
       type: new GraphQLNonNull(GraphQLString),
     },
   }),
 });
+
+export const ProfileChangeType = new GraphQLInputObjectType({
+  name: 'ProfileChange',
+  fields: () => ({
+    ...profileDTO,
+  }),
+});
+
+const memberTypeDTO = {
+  discount: {
+    type: GraphQLFloat,
+  },
+  monthPostsLimit: {
+    type: GraphQLInt,
+  },
+}
 
 export const MemberTypeType = new GraphQLObjectType({
   name: 'MemberType',
@@ -159,23 +150,13 @@ export const MemberTypeType = new GraphQLObjectType({
     id: {
       type: new GraphQLNonNull(GraphQLString),
     },
-    discount: {
-      type: GraphQLFloat,
-    },
-    monthPostsLimit: {
-      type: GraphQLInt,
-    },
+    ...memberTypeDTO
   }),
 });
 
-export const MemberTypeDTOType = new GraphQLInputObjectType({
-  name: 'MemberTypeDTO',
+export const MemberTypeChangeType = new GraphQLInputObjectType({
+  name: 'MemberTypeChange',
   fields: () => ({
-    discount: {
-      type: GraphQLFloat,
-    },
-    monthPostsLimit: {
-      type: GraphQLInt,
-    },
+    ...memberTypeDTO
   }),
 });
--- src/routes/graphql/mutationType.ts
+++ src/routes/graphql/mutationType.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { GraphQLObjectType } from "graphql";
-import { createPost } from "../../actions/postActions";
-import { createProfile } from "../../actions/profileActons";
-import { createUser } from "../../actions/userActions";
-import { PostDTOType, PostType, ProfileDTOType, ProfileType, UserDTOType, UserType } from "./model";
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { updateMemberType } from "../../actions/memberTypeAction";
+import { createPost, updatePost } from "../../actions/postActions";
+import { createProfile, updateProfile } from "../../actions/profileActons";
+import { createUser, updateUser, userSubscribeTo, userUnsubscribeTo } from "../../actions/userActions";
+import { MemberTypeChangeType, MemberTypeType, PostChangeType, PostCreateType, PostType, ProfileChangeType, ProfileCreateType, ProfileType, UserChangeType, UserCreateType, UserType } from "./model";
 
 export const mutationType = new GraphQLObjectType({
   name: 'Mutation',
@@ -14,7 +15,7 @@ export const mutationType = new GraphQLObjectType({
         userDTO: {
           description:
             'firstName, lastName, email of User',
-          type: UserDTOType,
+          type: UserCreateType,
         },
       },
       resolve: (_source, { userDTO }, context: FastifyInstance) => createUser(userDTO, context),
@@ -25,7 +26,7 @@ export const mutationType = new GraphQLObjectType({
         postDTO: {
           description:
             'title, content, userId of Post',
-          type: PostDTOType,
+          type: PostCreateType,
         },
       },
       resolve: (_source, { postDTO }, context: FastifyInstance) => createPost(postDTO, context),
@@ -36,10 +37,106 @@ export const mutationType = new GraphQLObjectType({
         profileDTO: {
           description:
             'avatar, sex, birthday, country, street, city, memberTypeId, userId of Profile',
-          type: ProfileDTOType,
+          type: ProfileCreateType,
         },
       },
       resolve: (_source, { profileDTO }, context: FastifyInstance) => createProfile(profileDTO, context),
     },
+    updateUser: {
+      type: UserType,
+      args: {
+        userDTO: {
+          description:
+            'firstName, lastName, email, subscribedToUserIds of User',
+          type: UserChangeType,
+        },
+        id: {
+          description:
+            'User Id',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_source, { id, userDTO }, context: FastifyInstance) => updateUser(id, userDTO, context),
+    },
+    updatePost: {
+      type: PostType,
+      args: {
+        postDTO: {
+          description:
+            'title, content, userId of Post',
+          type: PostChangeType,
+        },
+        id: {
+          description:
+            'Post Id',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_source, { id, postDTO }, context: FastifyInstance) => updatePost(id, postDTO, context),
+    },
+    updateProfile: {
+      type: ProfileType,
+      args: {
+        profileDTO: {
+          description:
+            'avatar, sex, birthday, country, street, city, memberTypeId, userId of Profile',
+          type: ProfileChangeType,
+        },
+        id: {
+          description:
+            'Profile Id',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_source, { id, profileDTO }, context: FastifyInstance) => updateProfile(id, profileDTO, context),
+    },
+    updateMemberType: {
+      type: MemberTypeType,
+      args: {
+        memberTypeDTO: {
+          description: 
+            'discount, monthPostsLimit of MemberType',
+          type: MemberTypeChangeType,
+        },
+        id: {
+          description:
+            'MemberType Id',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_source, { id, memberTypeDTO }, context: FastifyInstance) => updateMemberType(id, memberTypeDTO, context),
+    },
+    subscribeUser: {
+      type: UserType,
+      args: {
+        userId: {
+          description:
+            'User Id',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        subscribedId: {
+          description:
+            'Subscribed user Id',
+          type: new GraphQLNonNull(GraphQLString),
+        }
+      },
+      resolve:  (_source, { userId, subscribedId }, context: FastifyInstance) => userSubscribeTo(userId, subscribedId, context),
+    },
+    unsubscribeUser: {
+      type: UserType,
+      args: {
+        userId: {
+          description:
+            'User Id',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        unsubscribedId: {
+          description:
+            'Unsubscribed user Id',
+          type: new GraphQLNonNull(GraphQLString),
+        }
+      },
+      resolve:  (_source, { userId, unsubscribedId }, context: FastifyInstance) => userUnsubscribeTo(userId, unsubscribedId, context),
+    }
   }),
 });
