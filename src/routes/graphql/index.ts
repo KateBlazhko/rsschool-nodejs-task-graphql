0fa208769c5c0fe4c790fda3647ddaeb8fae3f28
Kate Blazhko <kateugnicheva@gmail.com>
2023-01-30 11:15:35 +0300

    feat: add validate

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,11 +1,13 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { FastifyInstance } from 'fastify';
-import { graphql, GraphQLSchema } from 'graphql';
+import { graphql, GraphQLSchema, parse } from 'graphql';
+import depthLimit = require('graphql-depth-limit');
 import { createLoaders } from '../../loaders/loaders';
 import { LoadersType } from '../../loaders/model';
 import { mutationType } from './mutationType';
 import { queryType } from './queryType';
 import { graphqlBodySchema } from './schema';
+import { validate } from 'graphql/validation'; 
 
 export type ContextType = LoadersType & {
   fastify: FastifyInstance;
@@ -21,15 +23,18 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-  
     async function (request, reply) {
       const schema = new GraphQLSchema({
         query: queryType,
-        mutation: mutationType
+        mutation: mutationType,
       });
-      const loaders = createLoaders(fastify)
 
       if (request.body.query) {
+        const documentAST = parse(request.body.query)
+        validate(schema, documentAST, [ depthLimit(10) ])
+
+        const loaders = createLoaders(fastify)
+
         return await graphql({schema, source: request.body.query, contextValue: {fastify, ...loaders}, variableValues: request.body.variables})
       }
 
